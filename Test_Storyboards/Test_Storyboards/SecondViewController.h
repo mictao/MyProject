57413897892d362994050ac06e919835791c8830
Michelle <mitchele.tao@gmail.com>
2013-02-13 14:25:15 -0700

    New Project
    New Project

---
+++ Test_Storyboards/Test_Storyboards/SecondViewController.h
@@ -0,0 +1,14 @@
+//
+//  SecondViewController.h
+//  Test_Storyboards
+//
+//  Created by Administrator on 13-02-11.
+//  Copyright (c) 2013 Administrator. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SecondViewController : UIViewController
+@property(nonatomic, copy) NSString *attributeName;
+
+@end
