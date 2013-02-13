3c2d73ddb39d9a21daf5b1bc907bbdced1c05aca
Adam Clark <adamclark@Adams-MacBook-Pro.local>
2013-02-13 16:04:19 -0700

    this is a test

---
+++ Test_Storyboards/Test_Storyboards/TestClass.h
@@ -0,0 +1,13 @@
+//
+//  TestClass.h
+//  Test_Storyboards
+//
+//  Created by Adam Clark on 2013-02-13.
+//  Copyright (c) 2013 Administrator. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestClass : NSObject
+
+@end
---
+++ Test_Storyboards/Test_Storyboards/TestClass.m
@@ -0,0 +1,13 @@
+//
+//  TestClass.m
+//  Test_Storyboards
+//
+//  Created by Adam Clark on 2013-02-13.
+//  Copyright (c) 2013 Administrator. All rights reserved.
+//
+
+#import "TestClass.h"
+
+@implementation TestClass
+
+@end
